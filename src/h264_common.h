30c6d1701a277ddf9e34b843cd8af43cb632a118
Chema Gonzalez <chemag@fb.com>
2021-07-22 07:51:41 -0700

    h264nal: add slice_type table
    Will be used by the slice parsers.

--- src/h264_common.h
+++ src/h264_common.h
@@ -54,6 +54,24 @@ enum NalUnitType : uint8_t {
 bool IsNalUnitTypeReserved(uint32_t nal_unit_type);
 bool IsNalUnitTypeUnspecified(uint32_t nal_unit_type);
 
+// Table 7-3
+enum SliceType : uint8_t {
+  P = 0,
+  B = 1,
+  I = 2,
+  SP = 3,
+  SI = 4,
+  // slice_type values in the range 5..9 specify, in addition to the coding
+  // type of the current slice, that all other slices of the current coded
+  // picture shall have a value of slice_type equal to the current value of
+  // slice_type or equal to the current value of slice_type - 5.
+  P_ALL = 5,
+  B_ALL = 6,
+  I_ALL = 7,
+  SP_ALL = 8,
+  SI_ALL = 9,
+};
+
 // Methods for parsing RBSP. See section 7.4 of the H264 spec.
 //
 // Decoding is simply a matter of finding any 00 00 03 sequence and removing
