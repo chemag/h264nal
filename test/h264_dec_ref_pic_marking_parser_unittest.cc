84d6ce38d1a9562a0a18680995e056a8f4782793
Chema Gonzalez <chemag@fb.com>
2021-07-23 08:21:12 -0700

    h264nal: add dec_ref_pic_marking() tests
    Added both IDR and non-IDR tests.
    
    Tested:
    
    Added unittest.
    
    ```
    $ ./test/h264_dec_ref_pic_marking_parser_unittest
    Running main() from /builddir/build/BUILD/googletest-release-1.10.0/googletest/src/gtest_main.cc
    [==========] Running 2 tests from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 2 tests from H264DecRefPicMarkingParserTest
    [ RUN      ] H264DecRefPicMarkingParserTest.TestSampleDecRefPicMarking601IDR
    [       OK ] H264DecRefPicMarkingParserTest.TestSampleDecRefPicMarking601IDR (0 ms)
    [ RUN      ] H264DecRefPicMarkingParserTest.TestSampleDecRefPicMarking601NonIDR
    [       OK ] H264DecRefPicMarkingParserTest.TestSampleDecRefPicMarking601NonIDR (0 ms)
    [----------] 2 tests from H264DecRefPicMarkingParserTest (0 ms total)
    
    [----------] Global test environment tear-down
    [==========] 2 tests from 1 test suite ran. (0 ms total)
    [  PASSED  ] 2 tests.
    ```

--- test/h264_dec_ref_pic_marking_parser_unittest.cc
+++ test/h264_dec_ref_pic_marking_parser_unittest.cc
@@ -19,25 +19,35 @@ class H264DecRefPicMarkingParserTest : public ::testing::Test {
   ~H264DecRefPicMarkingParserTest() override {}
 };
 
-#ifdef NOTDEF
-TEST_F(H264DecRefPicMarkingParserTest, TestSampleDecRefPicMarking1) {
+TEST_F(H264DecRefPicMarkingParserTest, TestSampleDecRefPicMarking601IDR) {
   // dec_ref_pic_marking
   // fuzzer::conv: data
-  const uint8_t buffer[] = {0x10, 0xc6};
+  const uint8_t buffer[] = {0x00};
   // fuzzer::conv: begin
-  auto dec_ref_pic_marking =
-      H264DecRefPicMarkingParser::ParseDecRefPicMarking(buffer, arraysize(buffer), 1, 0);
+  uint32_t nal_unit_type = NalUnitType::CODED_SLICE_OF_IDR_PICTURE_NUT;
+  auto dec_ref_pic_marking = H264DecRefPicMarkingParser::ParseDecRefPicMarking(
+      buffer, arraysize(buffer), nal_unit_type);
   // fuzzer::conv: end
 
   EXPECT_TRUE(dec_ref_pic_marking != nullptr);
 
-  EXPECT_EQ(7, dec_ref_pic_marking->luma_log2_weight_denom);
-  EXPECT_EQ(-1, dec_ref_pic_marking->delta_chroma_log2_weight_denom);
-  EXPECT_THAT(dec_ref_pic_marking->luma_weight_l0_flag,
-              ::testing::ElementsAreArray({0}));
-  EXPECT_THAT(dec_ref_pic_marking->chroma_weight_l0_flag,
-              ::testing::ElementsAreArray({0}));
+  EXPECT_EQ(0, dec_ref_pic_marking->no_output_of_prior_pics_flag);
+  EXPECT_EQ(0, dec_ref_pic_marking->long_term_reference_flag);
 }
-#endif
+
+TEST_F(H264DecRefPicMarkingParserTest, TestSampleDecRefPicMarking601NonIDR) {
+  // dec_ref_pic_marking
+  // fuzzer::conv: data
+  const uint8_t buffer[] = {0x00};
+  // fuzzer::conv: begin
+  uint32_t nal_unit_type = NalUnitType::CODED_SLICE_OF_NON_IDR_PICTURE_NUT;
+  auto dec_ref_pic_marking = H264DecRefPicMarkingParser::ParseDecRefPicMarking(
+      buffer, arraysize(buffer), nal_unit_type);
+  // fuzzer::conv: end
+
+  EXPECT_TRUE(dec_ref_pic_marking != nullptr);
+
+  EXPECT_EQ(0, dec_ref_pic_marking->adaptive_ref_pic_marking_mode_flag);
+}
 
 }  // namespace h264nal
