c8863247ad070982ecca79f4f7ddb2934394d4c6
Chema Gonzalez <chemag@fb.com>
2021-07-23 07:57:30 -0700

    h264nal: add 601 slice_header tests
    Added an IDR and a non-IDR test.
    
    Tested:
    
    Added unittest:
    
    ```
    $ ./test/h264_slice_header_parser_unittest
    Running main() from /builddir/build/BUILD/googletest-release-1.10.0/googletest/src/gtest_main.cc
    [==========] Running 2 tests from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 2 tests from H264SliceHeaderParserTest
    [ RUN      ] H264SliceHeaderParserTest.TestSampleSliceIDR601
    [       OK ] H264SliceHeaderParserTest.TestSampleSliceIDR601 (0 ms)
    [ RUN      ] H264SliceHeaderParserTest.TestSampleSliceNonIDR601
    [       OK ] H264SliceHeaderParserTest.TestSampleSliceNonIDR601 (0 ms)
    [----------] 2 tests from H264SliceHeaderParserTest (0 ms total)
    
    [----------] Global test environment tear-down
    [==========] 2 tests from 1 test suite ran. (1 ms total)
    [  PASSED  ] 2 tests.
    ```

--- test/h264_slice_header_parser_unittest.cc
+++ test/h264_slice_header_parser_unittest.cc
@@ -10,11 +10,10 @@
 #include "h264_bitstream_parser_state.h"
 #include "h264_common.h"
 #include "h264_dec_ref_pic_marking_parser.h"
-#include "h264_pred_weight_table_parser.h"
 #include "h264_pps_parser.h"
+#include "h264_pred_weight_table_parser.h"
 #include "h264_ref_pic_list_reordering_parser.h"
 #include "h264_sps_parser.h"
-
 #include "rtc_base/arraysize.h"
 #include "rtc_base/bit_buffer.h"
 
@@ -26,152 +25,200 @@ class H264SliceHeaderParserTest : public ::testing::Test {
   ~H264SliceHeaderParserTest() override {}
 };
 
-#ifdef NOTDEF
-TEST_F(H264SliceHeaderParserTest, TestSampleSlice) {
+TEST_F(H264SliceHeaderParserTest, TestSampleSliceIDR601) {
   // fuzzer::conv: data
   const uint8_t buffer[] = {
-      0xaf, 0x09, 0x40, 0xf3, 0xb8, 0xd5, 0x39, 0xba,
-      0x1f, 0xe4, 0xa6, 0x08, 0x5c, 0x6e, 0xb1, 0x8f,
-      0x00, 0x38, 0xf1, 0xa6, 0xfc, 0xf1, 0x40, 0x04,
-      0x3a, 0x86, 0xcb, 0x90, 0x74, 0xce, 0xf0, 0x46,
-      0x61, 0x93, 0x72, 0xd6, 0xfc, 0x35, 0xe3, 0xc5
+      0x88, 0x82, 0x06, 0x78, 0x8c, 0x50, 0x00, 0x1c,
+      0xab, 0x8e, 0x00, 0x02, 0xfb, 0x31, 0xc0, 0x00,
+      0x5f, 0x66, 0xfb, 0xef, 0xbe
   };
 
   // fuzzer::conv: begin
   // get some mock state
   H264BitstreamParserState bitstream_parser_state;
   auto sps = std::make_shared<H264SpsParser::SpsState>();
-  sps->sample_adaptive_offset_enabled_flag = 1;
-  sps->chroma_format_idc = 1;
+  sps->log2_max_frame_num_minus4 = 1;
+  sps->frame_mbs_only_flag = 1;
+  sps->pic_order_cnt_type = 2;
+  sps->delta_pic_order_always_zero_flag = 0;
+  sps->pic_width_in_mbs_minus1 = 0;
+  sps->pic_height_in_map_units_minus1 = 0;
   bitstream_parser_state.sps[0] = sps;
   auto pps = std::make_shared<H264PpsParser::PpsState>();
+  pps->pic_order_present_flag = 0;
+  pps->redundant_pic_cnt_present_flag = 0;
+  pps->weighted_pred_flag = 0;
+  pps->weighted_bipred_idc = 0;
+  pps->entropy_coding_mode_flag = 0;
+  pps->deblocking_filter_control_present_flag = 1;
+  pps->num_slice_groups_minus1 = 0;
+  pps->slice_group_map_type = 0;
+  pps->slice_group_change_rate_minus1 = 0;
+  ;
   bitstream_parser_state.pps[0] = pps;
 
-  auto slice_layer =
-      H264SliceHeaderParser::ParseSliceHeader(
-          buffer, arraysize(buffer), NalUnitType::IDR_W_RADL,
-          &bitstream_parser_state);
+  uint32_t nal_ref_idc = 3;
+  uint32_t nal_unit_type = NalUnitType::CODED_SLICE_OF_IDR_PICTURE_NUT;
+  auto slice_header = H264SliceHeaderParser::ParseSliceHeader(
+      buffer, arraysize(buffer), nal_ref_idc, nal_unit_type,
+      &bitstream_parser_state);
   // fuzzer::conv: end
 
-  EXPECT_TRUE(slice_layer != nullptr);
+  EXPECT_TRUE(slice_header != nullptr);
 
-  auto& slice_header = slice_layer->slice_header;
+  EXPECT_EQ(0, slice_header->first_mb_in_slice);
+  EXPECT_EQ(7, slice_header->slice_type);
+  EXPECT_EQ(0, slice_header->pic_parameter_set_id);
+  EXPECT_EQ(0, slice_header->frame_num);
+  EXPECT_EQ(0, slice_header->field_pic_flag);
+  EXPECT_EQ(0, slice_header->bottom_field_flag);
+  EXPECT_EQ(0, slice_header->idr_pic_id);
+  EXPECT_EQ(0, slice_header->pic_order_cnt_lsb);
+  EXPECT_EQ(0, slice_header->delta_pic_order_cnt_bottom);
+  EXPECT_EQ(0, slice_header->delta_pic_order_cnt.size());
+  EXPECT_EQ(0, slice_header->redundant_pic_cnt);
+  EXPECT_EQ(0, slice_header->direct_spatial_mv_pred_flag);
+  EXPECT_EQ(0, slice_header->num_ref_idx_active_override_flag);
+  EXPECT_EQ(0, slice_header->num_ref_idx_l0_active_minus1);
+  EXPECT_EQ(0, slice_header->num_ref_idx_l1_active_minus1);
 
-  EXPECT_EQ(1, slice_header->first_slice_in_pic_flag);
-  EXPECT_EQ(0, slice_header->no_output_of_prior_pics_flag);
-  EXPECT_EQ(0, slice_header->slice_pic_parameter_set_id);
-  EXPECT_EQ(2, slice_header->slice_type);
-  EXPECT_EQ(1, slice_header->slice_sao_luma_flag);
-  EXPECT_EQ(1, slice_header->slice_sao_chroma_flag);
-  EXPECT_EQ(9, slice_header->slice_qp_delta);
+  EXPECT_NE(nullptr, slice_header->ref_pic_list_reordering);
+  EXPECT_EQ(
+      0,
+      slice_header->ref_pic_list_reordering->ref_pic_list_reordering_flag_l0);
+  EXPECT_EQ(
+      0,
+      slice_header->ref_pic_list_reordering->ref_pic_list_reordering_flag_l1);
+  EXPECT_EQ(
+      0,
+      slice_header->ref_pic_list_reordering->reordering_of_pic_nums_idc.size());
+  EXPECT_EQ(
+      0, slice_header->ref_pic_list_reordering->abs_diff_pic_num_minus1.size());
+  EXPECT_EQ(0, slice_header->ref_pic_list_reordering->long_term_pic_num.size());
 
-#if 0
-  EXPECT_EQ(1, slice_header->alignment_bit_equal_to_one);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-#endif
+  EXPECT_EQ(nullptr, slice_header->pred_weight_table);
+
+  EXPECT_NE(nullptr, slice_header->dec_ref_pic_marking);
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->no_output_of_prior_pics_flag);
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->long_term_reference_flag);
+  EXPECT_EQ(
+      0, slice_header->dec_ref_pic_marking->adaptive_ref_pic_marking_mode_flag);
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking
+                   ->memory_management_control_operation.size());
+  EXPECT_EQ(
+      0,
+      slice_header->dec_ref_pic_marking->difference_of_pic_nums_minus1.size());
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->long_term_pic_num.size());
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->long_term_frame_idx.size());
+  EXPECT_EQ(
+      0,
+      slice_header->dec_ref_pic_marking->max_long_term_frame_idx_plus1.size());
+
+  EXPECT_EQ(0, slice_header->cabac_init_idc);
+  EXPECT_EQ(0, slice_header->sp_for_switch_flag);
+  EXPECT_EQ(-12, slice_header->slice_qp_delta);
+  EXPECT_EQ(0, slice_header->slice_qs_delta);
+  EXPECT_EQ(0, slice_header->disable_deblocking_filter_idc);
+  EXPECT_EQ(0, slice_header->slice_alpha_c0_offset_div2);
+  EXPECT_EQ(0, slice_header->slice_beta_offset_div2);
+  EXPECT_EQ(0, slice_header->slice_group_change_cycle);
 }
 
-TEST_F(H264SliceHeaderParserTest, TestSampleSlice2) {
+TEST_F(H264SliceHeaderParserTest, TestSampleSliceNonIDR601) {
+  // fuzzer::conv: data
   const uint8_t buffer[] = {
-      0x26, 0x01, 0x20, 0xf3, 0xc3, 0x5c, 0xfd, 0xfe,
-      0xd6, 0x25, 0xbc, 0x0d, 0xb1, 0xfa, 0x81, 0x63,
-      0xde, 0x0a, 0xc4, 0xa7, 0xea, 0x42, 0x89, 0xb6,
-      0x1e, 0xbb, 0x5e, 0x3f, 0xfd, 0x6c, 0x8a, 0x2d
+      0x9a, 0x1c, 0x0c, 0xf0, 0x09, 0x6c
   };
 
+  // fuzzer::conv: begin
   // get some mock state
   H264BitstreamParserState bitstream_parser_state;
   auto sps = std::make_shared<H264SpsParser::SpsState>();
-  sps->sample_adaptive_offset_enabled_flag = 1;
-  sps->chroma_format_idc = 1;
-  sps->log2_min_luma_coding_block_size_minus3 = 0;
-  sps->log2_diff_max_min_luma_coding_block_size = 2;
-  sps->pic_width_in_luma_samples = 1280;
-  sps->pic_height_in_luma_samples = 736;
+  sps->log2_max_frame_num_minus4 = 1;
+  sps->frame_mbs_only_flag = 1;
+  sps->pic_order_cnt_type = 2;
+  sps->delta_pic_order_always_zero_flag = 0;
+  sps->pic_width_in_mbs_minus1 = 0;
+  sps->pic_height_in_map_units_minus1 = 0;
   bitstream_parser_state.sps[0] = sps;
   auto pps = std::make_shared<H264PpsParser::PpsState>();
+  pps->pic_order_present_flag = 0;
+  pps->redundant_pic_cnt_present_flag = 0;
+  pps->weighted_pred_flag = 0;
+  pps->weighted_bipred_idc = 0;
+  pps->entropy_coding_mode_flag = 0;
+  pps->deblocking_filter_control_present_flag = 1;
+  pps->num_slice_groups_minus1 = 0;
+  pps->slice_group_map_type = 0;
+  pps->slice_group_change_rate_minus1 = 0;
+  ;
   bitstream_parser_state.pps[0] = pps;
 
-  auto slice_layer =
-      H264SliceHeaderParser::ParseSliceHeader(
-          buffer, arraysize(buffer), NalUnitType::IDR_W_RADL,
-          &bitstream_parser_state);
-  EXPECT_TRUE(slice_layer != nullptr);
+  uint32_t nal_ref_idc = 2;
+  uint32_t nal_unit_type = NalUnitType::CODED_SLICE_OF_NON_IDR_PICTURE_NUT;
+  auto slice_header = H264SliceHeaderParser::ParseSliceHeader(
+      buffer, arraysize(buffer), nal_ref_idc, nal_unit_type,
+      &bitstream_parser_state);
+  // fuzzer::conv: end
 
-  auto& slice_header = slice_layer->slice_header;
+  EXPECT_TRUE(slice_header != nullptr);
 
-  EXPECT_EQ(0, slice_header->first_slice_in_pic_flag);
-  EXPECT_EQ(0, slice_header->no_output_of_prior_pics_flag);
-  EXPECT_EQ(0, slice_header->slice_pic_parameter_set_id);
-  EXPECT_EQ(192, slice_header->slice_address);
-  EXPECT_EQ(3, slice_header->slice_type);
-  EXPECT_EQ(1, slice_header->slice_sao_luma_flag);
-  EXPECT_EQ(0, slice_header->slice_sao_chroma_flag);
-  EXPECT_EQ(15, slice_header->slice_qp_delta);
+  EXPECT_EQ(0, slice_header->first_mb_in_slice);
+  EXPECT_EQ(5, slice_header->slice_type);
+  EXPECT_EQ(0, slice_header->pic_parameter_set_id);
+  EXPECT_EQ(1, slice_header->frame_num);
+  EXPECT_EQ(0, slice_header->field_pic_flag);
+  EXPECT_EQ(0, slice_header->bottom_field_flag);
+  EXPECT_EQ(0, slice_header->idr_pic_id);
+  EXPECT_EQ(0, slice_header->pic_order_cnt_lsb);
+  EXPECT_EQ(0, slice_header->delta_pic_order_cnt_bottom);
+  EXPECT_EQ(0, slice_header->delta_pic_order_cnt.size());
+  EXPECT_EQ(0, slice_header->redundant_pic_cnt);
+  EXPECT_EQ(0, slice_header->direct_spatial_mv_pred_flag);
+  EXPECT_EQ(1, slice_header->num_ref_idx_active_override_flag);
+  EXPECT_EQ(0, slice_header->num_ref_idx_l0_active_minus1);
+  EXPECT_EQ(0, slice_header->num_ref_idx_l1_active_minus1);
 
-#if 0
-  EXPECT_EQ(1, slice_header->alignment_bit_equal_to_one);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-  EXPECT_EQ(0, slice_header->alignment_bit_equal_to_zero);
-#endif
+  EXPECT_NE(nullptr, slice_header->ref_pic_list_reordering);
+  EXPECT_EQ(
+      0,
+      slice_header->ref_pic_list_reordering->ref_pic_list_reordering_flag_l0);
+  EXPECT_EQ(
+      0,
+      slice_header->ref_pic_list_reordering->ref_pic_list_reordering_flag_l1);
+  EXPECT_EQ(
+      0,
+      slice_header->ref_pic_list_reordering->reordering_of_pic_nums_idc.size());
+  EXPECT_EQ(
+      0, slice_header->ref_pic_list_reordering->abs_diff_pic_num_minus1.size());
+  EXPECT_EQ(0, slice_header->ref_pic_list_reordering->long_term_pic_num.size());
+
+  EXPECT_EQ(nullptr, slice_header->pred_weight_table);
+
+  EXPECT_NE(nullptr, slice_header->dec_ref_pic_marking);
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->no_output_of_prior_pics_flag);
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->long_term_reference_flag);
+  EXPECT_EQ(
+      0, slice_header->dec_ref_pic_marking->adaptive_ref_pic_marking_mode_flag);
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking
+                   ->memory_management_control_operation.size());
+  EXPECT_EQ(
+      0,
+      slice_header->dec_ref_pic_marking->difference_of_pic_nums_minus1.size());
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->long_term_pic_num.size());
+  EXPECT_EQ(0, slice_header->dec_ref_pic_marking->long_term_frame_idx.size());
+  EXPECT_EQ(
+      0,
+      slice_header->dec_ref_pic_marking->max_long_term_frame_idx_plus1.size());
+
+  EXPECT_EQ(0, slice_header->cabac_init_idc);
+  EXPECT_EQ(0, slice_header->sp_for_switch_flag);
+  EXPECT_EQ(-12, slice_header->slice_qp_delta);
+  EXPECT_EQ(0, slice_header->slice_qs_delta);
+  EXPECT_EQ(0, slice_header->disable_deblocking_filter_idc);
+  EXPECT_EQ(0, slice_header->slice_alpha_c0_offset_div2);
+  EXPECT_EQ(0, slice_header->slice_beta_offset_div2);
+  EXPECT_EQ(0, slice_header->slice_group_change_cycle);
 }
-
-TEST_F(H264SliceHeaderParserTest, TestSampleSliceBroken) {
-  const uint8_t buffer[] = {
-			0x02, 0x01, 0xd2, 0x78, 0xf7, 0x55, 0xdc, 0xbf,
-			0x2c, 0x44, 0x00, 0x41, 0xa8, 0xd1, 0x66, 0x44,
-			0xb3, 0x12, 0x93, 0xbc, 0x96, 0x54, 0x62, 0x42,
-			0x53, 0xee, 0xd6, 0x14, 0x3a, 0xf4, 0x39, 0xfd,
-			0x9a, 0x61, 0x28, 0xe2, 0x52, 0x7b, 0x50, 0x48,
-			0x4d, 0x1c, 0xe3, 0xc5, 0x63, 0xe6, 0x7d, 0xa5,
-			0xb0, 0x87, 0xd4, 0x5f, 0x8a, 0x07, 0x4d, 0x99,
-			0x40, 0xcc, 0xe6, 0x21, 0xe3, 0x48, 0xc0, 0x06,
-			0x33, 0x2c, 0x0c, 0x04, 0x77, 0x39, 0x22, 0xb6,
-			0x7b, 0xe1, 0x40, 0x59, 0xec, 0x19, 0x04, 0xfc,
-			0x9a, 0xa5, 0x3f, 0x3a, 0x80, 0x5a, 0x62, 0x3d,
-			0xcf, 0xc1, 0x38, 0xe3, 0x8c, 0xed, 0xf1, 0x44,
-			0xa8, 0x54, 0xe9, 0x16, 0xf1, 0x22, 0x7b, 0x86,
-			0x3a, 0x8a, 0x7f, 0x9b, 0xac, 0x64, 0xe6, 0xa2,
-			0xd7, 0x0f, 0x3d, 0x6e, 0xe8, 0xc2, 0x0d, 0xa7,
-			0x48, 0xbd, 0x55, 0x53, 0x71, 0x89, 0x8b, 0x82,
-			0x59, 0x5c, 0xce, 0x18, 0x1e, 0xa5, 0x9f, 0xcd,
-			0x1a, 0xdd, 0x1e, 0x81, 0x69, 0x3d, 0x2d, 0x2d,
-			0xb2, 0xab, 0x29, 0xdc, 0xdf, 0x65, 0x28, 0x48,
-			0x34, 0x46, 0x01, 0x56, 0xdd, 0xae, 0x93, 0x19,
-  };
-
-  // get some mock state
-  H264BitstreamParserState bitstream_parser_state;
-  auto sps = std::make_shared<H264SpsParser::SpsState>();
-  sps->sample_adaptive_offset_enabled_flag = 1;
-  sps->chroma_format_idc = 1;
-  sps->log2_min_luma_coding_block_size_minus3 = 0;
-  sps->log2_diff_max_min_luma_coding_block_size = 2;
-  sps->pic_width_in_luma_samples = 1280;
-  sps->pic_height_in_luma_samples = 736;
-  bitstream_parser_state.sps[0] = sps;
-  auto pps = std::make_shared<H264PpsParser::PpsState>();
-  bitstream_parser_state.pps[0] = pps;
-
-  auto slice_layer =
-      H264SliceHeaderParser::ParseSliceHeader(
-          buffer, arraysize(buffer), NalUnitType::IDR_W_RADL,
-          &bitstream_parser_state);
-  EXPECT_TRUE(slice_layer != nullptr);
-
-  // invalid slice_header
-  auto& slice_header = slice_layer->slice_header;
-  EXPECT_TRUE(slice_header == nullptr);
-}
-#endif  // NOTDEF
 
 }  // namespace h264nal
