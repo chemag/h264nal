ccb34629f53457410507c7eeec297c7f71ccc806
Chema Gonzalez <chemag@fb.com>
2021-07-24 04:35:56 -0700

    h264nal: add pred_weight_table() test
    Added s simple test.
    
    Tested:
    
    Added unittest.
    
    ```
    $ ./test/h264_pred_weight_table_parser_unittest
    Running main() from /builddir/build/BUILD/googletest-release-1.10.0/googletest/src/gtest_main.cc
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from H264PredWeightTableParserTest
    [ RUN      ] H264PredWeightTableParserTest.TestSamplePredWeightTable1
    [       OK ] H264PredWeightTableParserTest.TestSamplePredWeightTable1 (0 ms)
    [----------] 1 test from H264PredWeightTableParserTest (0 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test suite ran. (0 ms total)
    [  PASSED  ] 1 test.
    ```

--- test/h264_pred_weight_table_parser_unittest.cc
+++ test/h264_pred_weight_table_parser_unittest.cc
@@ -19,25 +19,40 @@ class H264PredWeightTableParserTest : public ::testing::Test {
   ~H264PredWeightTableParserTest() override {}
 };
 
-#ifdef NOTDEF
 TEST_F(H264PredWeightTableParserTest, TestSamplePredWeightTable1) {
   // pred_weight_table
   // fuzzer::conv: data
-  const uint8_t buffer[] = {0x10, 0xc6};
+  const uint8_t buffer[] = {0x6c};
   // fuzzer::conv: begin
+  uint32_t slice_type = SliceType::P_ALL;
+  uint32_t num_ref_idx_l0_active_minus1 = 0;
+  uint32_t num_ref_idx_l1_active_minus1 = 0;
+
   auto pred_weight_table = H264PredWeightTableParser::ParsePredWeightTable(
-      buffer, arraysize(buffer), 1, 0);
+      buffer, arraysize(buffer), slice_type, num_ref_idx_l0_active_minus1,
+      num_ref_idx_l1_active_minus1);
   // fuzzer::conv: end
 
   EXPECT_TRUE(pred_weight_table != nullptr);
 
-  EXPECT_EQ(7, pred_weight_table->luma_log2_weight_denom);
-  EXPECT_EQ(-1, pred_weight_table->delta_chroma_log2_weight_denom);
+  EXPECT_EQ(2, pred_weight_table->luma_log2_weight_denom);
+  EXPECT_EQ(2, pred_weight_table->chroma_log2_weight_denom);
+  EXPECT_EQ(1, pred_weight_table->luma_weight_l0_flag.size());
   EXPECT_THAT(pred_weight_table->luma_weight_l0_flag,
               ::testing::ElementsAreArray({0}));
+  EXPECT_EQ(0, pred_weight_table->luma_weight_l0.size());
+  EXPECT_EQ(0, pred_weight_table->luma_offset_l0.size());
+  EXPECT_EQ(1, pred_weight_table->chroma_weight_l0_flag.size());
   EXPECT_THAT(pred_weight_table->chroma_weight_l0_flag,
               ::testing::ElementsAreArray({0}));
+  EXPECT_EQ(0, pred_weight_table->chroma_weight_l0.size());
+  EXPECT_EQ(0, pred_weight_table->chroma_offset_l0.size());
+  EXPECT_EQ(0, pred_weight_table->luma_weight_l1_flag.size());
+  EXPECT_EQ(0, pred_weight_table->luma_weight_l1.size());
+  EXPECT_EQ(0, pred_weight_table->luma_offset_l1.size());
+  EXPECT_EQ(0, pred_weight_table->chroma_weight_l1_flag.size());
+  EXPECT_EQ(0, pred_weight_table->chroma_weight_l1.size());
+  EXPECT_EQ(0, pred_weight_table->chroma_offset_l1.size());
 }
-#endif
 
 }  // namespace h264nal
