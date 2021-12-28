63ec127f3213b47fd20d5b63c3e583b2727006bf
Chema Gonzalez <chemag@fb.com>
2021-12-28 10:18:07 -0800

    h264nal: split the classes in h264_nal_unit_parser.*
    Moved the 3x classes to different files. This will allow the
    integration of slice_header_in_scalable_extension() RBSPs
    (type 20).
    
    Tested:
    
    Ran unittest.
    
    ```
    $ make test
    Running tests...
    Test project h264nal/build
          Start  1: h264_common_unittest
     1/22 Test  #1: h264_common_unittest .........................................   Passed    0.00 sec
          Start  2: h264_hrd_parameters_parser_unittest
     2/22 Test  #2: h264_hrd_parameters_parser_unittest ..........................   Passed    0.00 sec
          Start  3: h264_vui_parameters_parser_unittest
     3/22 Test  #3: h264_vui_parameters_parser_unittest ..........................   Passed    0.00 sec
          Start  4: h264_sps_parser_unittest
     4/22 Test  #4: h264_sps_parser_unittest .....................................   Passed    0.00 sec
          Start  5: h264_pps_parser_unittest
     5/22 Test  #5: h264_pps_parser_unittest .....................................   Passed    0.00 sec
          Start  6: h264_ref_pic_list_modification_parser_unittest
     6/22 Test  #6: h264_ref_pic_list_modification_parser_unittest ...............   Passed    0.00 sec
          Start  7: h264_pred_weight_table_parser_unittest
     7/22 Test  #7: h264_pred_weight_table_parser_unittest .......................   Passed    0.00 sec
          Start  8: h264_dec_ref_pic_marking_parser_unittest
     8/22 Test  #8: h264_dec_ref_pic_marking_parser_unittest .....................   Passed    0.00 sec
          Start  9: h264_rtp_single_parser_unittest
     9/22 Test  #9: h264_rtp_single_parser_unittest ..............................   Passed    0.00 sec
          Start 10: h264_rtp_stapa_parser_unittest
    10/22 Test #10: h264_rtp_stapa_parser_unittest ...............................   Passed    0.00 sec
          Start 11: h264_rtp_fua_parser_unittest
    11/22 Test #11: h264_rtp_fua_parser_unittest .................................   Passed    0.00 sec
          Start 12: h264_rtp_parser_unittest
    12/22 Test #12: h264_rtp_parser_unittest .....................................   Passed    0.00 sec
          Start 13: h264_slice_header_parser_unittest
    13/22 Test #13: h264_slice_header_parser_unittest ............................   Passed    0.00 sec
          Start 14: h264_slice_header_in_scalable_extension_parser_unittest
    14/22 Test #14: h264_slice_header_in_scalable_extension_parser_unittest ......   Passed    0.00 sec
          Start 15: h264_slice_layer_without_partitioning_rbsp_parser_unittest
    15/22 Test #15: h264_slice_layer_without_partitioning_rbsp_parser_unittest ...   Passed    0.00 sec
          Start 16: h264_slice_layer_extension_rbsp_parser_unittest
    16/22 Test #16: h264_slice_layer_extension_rbsp_parser_unittest ..............   Passed    0.00 sec
          Start 17: h264_subset_sps_parser_unittest
    17/22 Test #17: h264_subset_sps_parser_unittest ..............................   Passed    0.00 sec
          Start 18: h264_sps_svc_extension_parser_unittest
    18/22 Test #18: h264_sps_svc_extension_parser_unittest .......................   Passed    0.00 sec
          Start 19: h264_bitstream_parser_unittest
    19/22 Test #19: h264_bitstream_parser_unittest ...............................   Passed    0.00 sec
          Start 20: h264_prefix_nal_unit_parser_unittest
    20/22 Test #20: h264_prefix_nal_unit_parser_unittest .........................   Passed    0.00 sec
          Start 21: h264_nal_unit_header_svc_extension_parser_unittest
    21/22 Test #21: h264_nal_unit_header_svc_extension_parser_unittest ...........   Passed    0.00 sec
          Start 22: h264_nal_unit_parser_unittest
    22/22 Test #22: h264_nal_unit_parser_unittest ................................   Passed    0.00 sec
    
    100% tests passed, 0 tests failed out of 22
    
    Total Test time (real) =   0.08 sec
    
    ```

--- src/h264_nal_unit_parser.h
+++ src/h264_nal_unit_parser.h
@@ -10,97 +10,11 @@
 
 #include "h264_bitstream_parser_state.h"
 #include "h264_common.h"
-#include "h264_nal_unit_header_svc_extension_parser.h"
-#include "h264_pps_parser.h"
-#include "h264_prefix_nal_unit_parser.h"
-#include "h264_slice_layer_without_partitioning_rbsp_parser.h"
-#include "h264_sps_parser.h"
-#include "h264_subset_sps_parser.h"
+#include "h264_nal_unit_header_parser.h"
+#include "h264_nal_unit_payload_parser.h"
 #include "rtc_base/bit_buffer.h"
 
 namespace h264nal {
-
-// A class for parsing out an H264 NAL Unit Header.
-class H264NalUnitHeaderParser {
- public:
-  // The parsed state of the NAL Unit Header.
-  struct NalUnitHeaderState {
-    NalUnitHeaderState() = default;
-    ~NalUnitHeaderState() = default;
-    // disable copy ctor, move ctor, and copy&move assignments
-    NalUnitHeaderState(const NalUnitHeaderState&) = delete;
-    NalUnitHeaderState(NalUnitHeaderState&&) = delete;
-    NalUnitHeaderState& operator=(const NalUnitHeaderState&) = delete;
-    NalUnitHeaderState& operator=(NalUnitHeaderState&&) = delete;
-
-#ifdef FDUMP_DEFINE
-    void fdump(FILE* outfp, int indent_level) const;
-#endif  // FDUMP_DEFINE
-
-    uint32_t forbidden_zero_bit = 0;
-    uint32_t nal_ref_idc = 0;
-    uint32_t nal_unit_type = 0;
-    uint32_t svc_extension_flag = 0;
-    uint32_t avc_3d_extension_flag = 0;
-    std::unique_ptr<struct H264NalUnitHeaderSvcExtensionParser::
-                        NalUnitHeaderSvcExtensionState>
-        nal_unit_header_svc_extension;
-    // TODO(chema): nal_unit_header_3davc_extension()  // specified in Annex J
-    // TODO(chema): nal_unit_header_mvc_extension()  // specified in Annex H
-  };
-
-  // Unpack RBSP and parse NAL unit header state from the supplied buffer.
-  static std::unique_ptr<NalUnitHeaderState> ParseNalUnitHeader(
-      const uint8_t* data, size_t length) noexcept;
-  static std::unique_ptr<NalUnitHeaderState> ParseNalUnitHeader(
-      rtc::BitBuffer* bit_buffer) noexcept;
-  // Parses nalu type from the given buffer
-  static bool GetNalUnitType(const uint8_t* data, const size_t length,
-                             NalUnitType& naluType) noexcept;
-};
-
-// A class for parsing out an H264 NAL Unit Payload.
-class H264NalUnitPayloadParser {
- public:
-  // The parsed state of the NAL Unit Payload. Only some select values are
-  // stored. Add more as they are actually needed.
-  struct NalUnitPayloadState {
-    NalUnitPayloadState() = default;
-    ~NalUnitPayloadState() = default;
-    // disable copy ctor, move ctor, and copy&move assignments
-    NalUnitPayloadState(const NalUnitPayloadState&) = delete;
-    NalUnitPayloadState(NalUnitPayloadState&&) = delete;
-    NalUnitPayloadState& operator=(const NalUnitPayloadState&) = delete;
-    NalUnitPayloadState& operator=(NalUnitPayloadState&&) = delete;
-
-#ifdef FDUMP_DEFINE
-    void fdump(FILE* outfp, int indent_level, uint32_t nal_unit_type) const;
-#endif  // FDUMP_DEFINE
-
-    std::shared_ptr<struct H264SpsParser::SpsState> sps;
-    std::shared_ptr<struct H264PpsParser::PpsState> pps;
-    std::unique_ptr<struct H264SliceLayerWithoutPartitioningRbspParser::
-                        SliceLayerWithoutPartitioningRbspState>
-        slice_layer_without_partitioning_rbsp;
-    std::unique_ptr<struct H264PrefixNalUnitRbspParser::PrefixNalUnitRbspState>
-        prefix_nal_unit;
-    std::shared_ptr<struct H264SubsetSpsParser::SubsetSpsState> subset_sps;
-  };
-
-  // Unpack RBSP and parse NAL unit payload state from the supplied buffer.
-  static std::unique_ptr<NalUnitPayloadState> ParseNalUnitPayload(
-      const uint8_t* data, size_t length,
-      H264NalUnitHeaderParser::NalUnitHeaderState& nal_unit_header,
-      struct H264BitstreamParserState* bitstream_parser_state) noexcept;
-  static std::unique_ptr<NalUnitPayloadState> ParseNalUnitPayload(
-      rtc::BitBuffer* bit_buffer,
-      H264NalUnitHeaderParser::NalUnitHeaderState& nal_unit_header,
-      struct H264BitstreamParserState* bitstream_parser_state) noexcept;
-  // used by RTP fu-a, which has a pseudo-NALU header
-  static std::unique_ptr<NalUnitPayloadState> ParseNalUnitPayload(
-      rtc::BitBuffer* bit_buffer, uint32_t nal_ref_idc, uint32_t nal_unit_type,
-      struct H264BitstreamParserState* bitstream_parser_state) noexcept;
-};
 
 // A class for parsing out an H264 NAL Unit.
 class H264NalUnitParser {
