30bee8ee7ea58e8f1af51b36e4e66fba5043396b
Chema Gonzalez <chemag@fb.com>
2025-06-04 16:10:13 -0700

    h264nal: fix stdin reading
    Tested:
    ```
    $ ffmpeg -i in.mp4 -c:v copy -f h264 - | h264nal --no-as-one-line -i -
    nal_unit {
      nal_unit_header {
        forbidden_zero_bit: 0
        nal_ref_idc: 0
        nal_unit_type: 6
      }
      nal_unit_payload {
    
      }
    }
    nal_unit {
      nal_unit_header {
        forbidden_zero_bit: 0
        nal_ref_idc: 1
        nal_unit_type: 7
    ...
    ```
    
    And
    ```
    $ ffmpeg -i in.mp4 -c:v copy -f h264 - > /tmp/foo.264
    ...
    $ cat /tmp/foo.264 | h264nal --no-as-one-line -i -
    nal_unit {
      nal_unit_header {
        forbidden_zero_bit: 0
        nal_ref_idc: 0
        nal_unit_type: 6
      }
      nal_unit_payload {
    
      }
    }
    ...
    ```
    
    ```
    $ h264nal --no-as-one-line -i /tmp/foo.264
    nal_unit {
      nal_unit_header {
        forbidden_zero_bit: 0
        nal_ref_idc: 0
        nal_unit_type: 6
      }
      nal_unit_payload {
    
      }
    }
    ...
    ```

--- src/h264_utils.cc
+++ src/h264_utils.cc
@@ -8,8 +8,23 @@
 
 #include <cstdint>
 #include <cstring>
+#include <iostream>
 #include <memory>
 #include <vector>
+
+namespace {
+int ReadStdinToBuffer(std::vector<uint8_t>& buffer) {
+  constexpr size_t chunk_size = 4096;
+  uint8_t temp[chunk_size];
+  while (true) {
+    std::cin.read(reinterpret_cast<char*>(temp), chunk_size);
+    std::streamsize bytes_read = std::cin.gcount();
+    if (bytes_read <= 0) break;
+    buffer.insert(buffer.end(), temp, temp + bytes_read);
+  }
+  return 0;
+}
+}  // namespace
 
 namespace h264nal {
 
@@ -17,10 +32,12 @@ int H264Utils::ReadFile(const char* filename, std::vector<uint8_t>& buffer) {
   // TODO(chemag): read the infile incrementally
   FILE* infp = nullptr;
   if ((filename == nullptr) || (strlen(filename) == 1 && filename[0] == '-')) {
-    infp = stdin;
-  } else {
-    infp = fopen(filename, "rb");
+    // read from stdin
+    return ReadStdinToBuffer(buffer);
   }
+
+  // open the file
+  infp = fopen(filename, "rb");
   if (infp == nullptr) {
     // did not work
     fprintf(stderr, "Could not open input file: \"%s\"\n", filename);
